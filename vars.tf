79d7eeb8a0b4772898aa098d03b9b2b788de3c54
sergevoinich <sergevoinich@gmail.com>
2019-01-14 17:40:40 -0500

    Added provisioners on packer image template. Simple ansible role

--- vars.tf
+++ vars.tf
@@ -5,6 +5,7 @@ variable "AWS_REGION" {
 variable "PATH_TO_PRIVATE_KEY" {
   default = "mykey"
 }
+
 variable "PATH_TO_PUBLIC_KEY" {
   default = "mykey.pub"
 }
@@ -15,9 +16,10 @@ variable "aws_secret_key" {}
 
 variable "AMIS" {
   type = "map"
+
   default = {
     us-east-2 = "ami-0cf8cc36b8c81c6de"
   }
 }
 
-variable "RDS_PASSWORD" { }
+variable "RDS_PASSWORD" {}
